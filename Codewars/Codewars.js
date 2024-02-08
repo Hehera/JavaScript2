9c1a3f97f14daebf554934063e2bb9fadd940467
Гегера Назар <lolerkapich335@gmail.com>
2024-02-08 23:49:48 +0200

    add libs JS folder

--- Codewars/Codewars.js
+++ Codewars/Codewars.js
@@ -1,2 +1,145 @@
+function makeNegative(num) {
+   return num > 0 ? -num : num
+}
+// console.log(makeNegative(43));//-43
+
+function positiveSum(arr) {
+   return arr.filter(num => num > 0).reduce((acc, cur) => acc + cur)
+}
+// console.log(positiveSum([1,-2,3,4,5]));//13
+
+function solution(str) {
+   let res = ''
+   for (let i = str.length - 1; i >= 0; i--) {
+      res += str[i]
+   }
+   return res
+}
+// console.log(solution('world'));//'world'  =>  'dlrow'
+
+function boolToWord(bool) {
+   return bool ? 'Yes' : 'No'
+}
+// console.log(boolToWord(false))
+// very simple, given a number (integer / decimal /
+// both depending on the language), find its opposite (additive inverse).
+
+function opposite(number) {
+   return number ? -number : number
+}
+//   console.log(opposite(1));
+
+function squareSum(numbers) {
+   return numbers.map(num => num * num).reduce((acc, curr) => acc + curr)
+}
+//   console.log(squareSum([0, 3, 4, 5]));
+
+function repeatStr(n, s) {
+   return s.repeat(n)
+}
+// console.log(repeatStr(3, '*'))//"***"
+
+const summation = num => {
+   let res = 0
+   for (let i = 0; i <= num; i++) {
+      res += i
+   }
+   return res
+}
+// console.log(summation(8));
+
+const noSpace = x => {
+   return x.replaceAll(' ', '')
+}
+// console.log(noSpace('dwq dqwd dqwd'));
+
+const findSmallestInt = args => {
+   return Math.min(...args)
+}
+//  console.log(findSmallestInt([78, 56, 232, 12, 8]))
+// if (operation === '+') {
+//    return value1 + value2
+// } else if (operation === '-') {
+//    return value1 - value2
+// } else if (operation === '/') {
+//    return value1 / value2
+// } else if (operation === '*') {
+//    return value1 * value2
+// }
+
+function basicOp(operation, value1, value2) {
+   const operator = {
+      '+': (a, b) => a + b,
+      '-': (a, b) => a - b,
+      '/': (a, b) => a / b,
+      '*': (a, b) => a * b
+   }
+
+   if (operator.hasOwnProperty(operation)) {
+      return operator[operation](value1, value2)
+   } else {
+      return 'error'
+   }
+}
+
+// console.log(basicOp("+", 4, 7));//11, "4 + 7 = 11"
+
+function getCount(str) {
+   return str.split('').filter(c => 'aeiouAEIOU'.includes(c)).length
+}
+// console.log(getCount('abracadabra')) //5
+
+// 1.Disemvowel Trolls
+
+function disemvowel(str) {
+   let res = ''
+   let vowels = ['a', 'o', 'u', 'e', 'i', 'A', 'O', 'U', 'E', 'I']
+
+   for (let i = 0; i < str.length; i++) {
+      if (!vowels.includes(str[i])) {
+         res += str[i]
+      }
+   }
+   return res
+}
+// console.log(disemvowel('kMxCjts Nxt tXT'))
+
+// 2. Square Every Digit
+
+function squareDigits(num) {
+   let res = 0
+   let stringed = num.toString()
+   let splited = stringed.split('')
+   let mapped = splited.map(num => num ** 2)
+
+   return parseInt(mapped.join(''), 10)
+
+// return +num.toString().split('').map(i => i*i).join('');
+}
+// console.log(squareDigits(9414))
 
 
+// 3 Highest and Lowest
+function highAndLow(numbers) {
+   let numArray = numbers.split(' ').map(Number)
+   let max = Math.max(...numArray)
+   let min = Math.min(...numArray)
+   return `${max} ${min}`
+}
+// console.log(highAndLow('1 2 3'))
+  
+
+// 4 Descending Order
+
+function descendingOrder(n) {
+return parseInt(n.toString().split('').sort((a,b) => b - a).map(Number).join(''))
+}
+console.log(descendingOrder(123456789))
+  
+// 5 Get the Middle Character
+
+function getMiddle(s) {
+}
+console.log(getMiddle(test));//es
+
+// Test.assertEquals(getMiddle("testing"),"t");
---
+++ LearnJS/Object.js
@@ -0,0 +1,133 @@
+const user = {
+   id: 1,
+   name: 'Sam Green',
+   age: 20
+}
+const keys = Object.keys(user)
+// console.log(keys)
+keys.forEach(key => {
+   //    console.log(user[key])
+})
+
+// The course detail(x) is Learn JavaScript(y)(course.name)
+
+const getCourseDetail = (course, detail) => {
+   return `The course ${detail} is ${course[detail]}`
+}
+// console.log(getCourseDetail({id: 1, name: "Learn JavaScript", year: 2021}, "name")); // "The course name is Learn JavaScript"
+// console.log(getCourseDetail({id: 1, name: "Learn JavaScript", year: 2021}, "year")); // "The course year is 2021"
+
+const settings = {
+   id: 1,
+   name: 'Learn JavaScript',
+   year: 2021
+}
+const setting = 'id'
+// console.log(settings[setting]);// 1
+
+const logValues = course => {
+   Object.values(course).forEach(value => {
+      return console.log(value)
+   })
+}
+// Sample usage - do not modify
+// console.log(logValues({id: 1, name: "Learn JavaScript", year: 2021}));
+
+const translations = {
+   welcome: {
+      dutch: 'Welkom',
+      french: 'Bienvenue',
+      english: 'Welcome'
+   }
+}
+const getTranslation = language => {
+   //    console.log(translations)
+   //    console.log(language)
+}
+// console.log(getTranslation());
+
+const getUpperCasedValues = course => {
+   return Object.values(course).map(value => {
+      return value.toUpperCase()
+   })
+}
+
+
+
+// Sample usage - do not modify
+// console.log(getUpperCasedValues({name: "Learn JavaScript"})); // ["LEARN JAVASCRIPT"]
+// console.log(getUpperCasedValues({name: "Learn JavaScript", category: "Programming"})); // ["LEARN JAVASCRIPT", "PROGRAMMING"]
+// console.log(getUpperCasedValues({})); // []
+
+
+
+
+const firstOne = false
+const secondOne= true
+
+const settings2 = {
+    firstOne,
+    secondOne,
+}
+// console.log(settings2);
+
+// Debugging tip
+
+const sum = (a, b) => {
+   console.log({a})//такий запис дає нам не просто бачити результат,але й зміну,яка дала цей результат
+   console.log({b})
+   let total = a + b
+   console.log({total})
+   return total
+}
+
+// sum(1, 3)   
+
+
+const config = {
+   id: 1,
+   isAdmin: false,
+   theme: {
+      dark: false,
+      accessibility: true
+   }
+}
+
+// const logged = config.theme.dark
+// This can be refactored with object destructuring as follows:
+
+// const {theme,id,isAdmin} = config
+const {id, isAdmin, theme} = config;
+// console.log(id,isAdmin,theme);
+
+
+
+const { a: aa = 10, b: bb = 5 } = { a: 3 };
+
+// console.log(aa); // 3
+// console.log(bb); // 5
+
+const firstPerson = {
+    name: "Sam",
+    age: 18
+}
+
+const secondPerson = {
+    age: 25,
+    type: "admin"
+}
+
+const mergedObjects = {...firstPerson, ...secondPerson};
+// console.log(mergedObjects);
+// Notice how the new object ended up with the name and type from both objects. However, regarding the age, since it exists in both objects, only the 2nd one persisted.
+// This is why the order of the objects matters when spreading them.
+
+
+const person = {
+    name: "Alex",
+    age: 35
+};
+
+if ("name" in person) {
+    // console.log(person.name);
+}
