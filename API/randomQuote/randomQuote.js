6d358d2f5479dea7574bdba39307b922cc98431b
Гегера Назар <lolerkapich335@gmail.com>
2024-02-12 13:42:18 +0200

    add randomQuote html-css-js

--- API/randomQuote/randomQuote.js
+++ API/randomQuote/randomQuote.js
@@ -1,1 +1,25 @@
-console.log('123')
+const url = 'https://api.quotable.io/quotes/random'
+const btn = document.getElementById('btn')
+const quote = document.getElementById('quote')
+const author = document.getElementById('author')
+const container = document.querySelector('.container')
+
+const getQuote = () => {
+   quote.classList.remove('fade')
+   author.classList.remove('fade')
+   fetch(url)
+      .then(data => data.json())
+      .then(item => {
+         console.log(item)
+         quote.textContent = `"${item[0].content}"`
+         author.textContent = `${item[0].author}`
+         quote.classList.add('fade')
+         author.classList.add('fade')
+      })
+}
+getQuote()
+window.addEventListener('load', getQuote) //Це гарантує, що коли веб-сторінка буде повністю завантажена та готова до взаємодії,
+// функція getQuote() буде виконана, отримуючи випадкову цитату та оновлюючи відображену цитату та автора на веб-сторінці.Це гарантує,
+// що коли веб-сторінка буде повністю завантажена та готова до взаємодії, функція getQuote() буде виконана, отримуючи випадкову цитату
+//  та оновлюючи відображену цитату та автора на веб-сторінці.
+btn.addEventListener('click', getQuote)
